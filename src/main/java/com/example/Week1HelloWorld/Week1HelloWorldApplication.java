823f48d116db07a4859833523e62e904d2f1e624
Shadows <36662635+RoyalBladeV2@users.noreply.github.com>
2020-09-17 15:25:38 +0300

    First commit

---
+++ src/main/java/com/example/Week1HelloWorld/Week1HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.Week1HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1HelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Week1HelloWorld/Week1HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Week1HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
